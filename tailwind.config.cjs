3c49f6670e9aff40933b5a17383f1a7d8c04f92b
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-02-28 00:24:40 +0900

    style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: [
+					'-apple-system',
+					"BlinkMacSystemFont",
+					"Hiragino Kaku Gothic ProN",
+					"Hiragino Sans",
+					"Meiryo",
+					"sans-serif",
+					"Segoe UI Emoji"
+				],
+				serif: [
+					'-apple-system',
+					"BlinkMacSystemFont",
+					"Hiragino Kaku Gothic ProN",
+					"Hiragino Sans",
+					"Meiryo",
+					"sans-serif",
+					"Segoe UI Emoji"
+				]
+			}
+		},
 	},
 	plugins: [],
 }
