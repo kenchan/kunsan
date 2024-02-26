399682b4feb372f4646eb6ffe993a5bdd9dbcd62
Kenichi Takahashi <kenichi.taka@gmail.com>
2024-02-26 22:14:50 +0900

    npx astro add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'server',
   adapter: vercel({
     analytics: true
@@ -17,7 +19,7 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'github-light',
-      wrap: true,
+      wrap: true
     }
   }
 });
